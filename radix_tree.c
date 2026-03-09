e0d266201b88fb23bc8bac4d815ab5de7d5fad99
Morris <morrislin13@gmail.com>
2026-03-09 15:15:14 -0400

    implemented insert for radix tree

--- radix_tree.c
+++ radix_tree.c
@@ -20,14 +20,14 @@ union radix_slot {
 };
 
 struct radix_tree {
-    uint child_cnt;
+    bool has_children;
     union radix_slot slot; 
 };
 
 #define BITS_IN_PTR ((sizeof(void*)) * 8)
 const int max_level = BITS_IN_PTR / TREE_WIDTH;
 
-struct range* entry_range(void *entry) {
+struct range* _get_entry_range(void *entry) {
     return (struct range*) entry;
 }
 
@@ -35,26 +35,35 @@ bool ranges_overlap(struct range *a, struct range *b) {
     return (a->left > b->right) || (b->left > a->right);
 }
 
+inline int _get_idx_in_node(int entry_pos, int level, int parent_bits) {
+    int entry_parent_bits = entry_pos >> ((max_level - level) * TREE_WIDTH);
+    bool entry_in_node = (entry_parent_bits == parent_bits);
+    if (!entry_in_node) {
+        return (entry_parent_bits < parent_bits)? 0 : (1 << TREE_WIDTH) - 1;
+    }
+    return (uintptr_t) entry_pos >> ((max_level - 1 - level) * TREE_WIDTH);
+}
+
+inline struct range _get_idx_range(struct range range, int level, int parent_idx_bits) {
+    int left = _get_idx_in_node(range.left, level, parent_idx_bits);
+    int right = _get_idx_in_node(range.right, level, parent_idx_bits);
+    struct range ans = {
+        .left = left,
+        .right = right,
+    };
+    return ans;
+}
+
 void* _is_empty_range(struct range range, struct radix_tree* cur, int level, uint parent_idx_bits) {
     if (cur == NULL) {
         return NULL;
     }
-    if (cur->child_cnt != 0) {
+    if (!cur->has_children) {
         return cur->slot.data;
     }
 
-    int left_idx = 0;
-    int right_idx = (1 << TREE_WIDTH) - 1;
-    bool left_bound_in_node = range.left >> (level * TREE_WIDTH) == parent_idx_bits;
-    bool right_bound_in_node = range.right >> (level * TREE_WIDTH) == parent_idx_bits;
-    if (left_bound_in_node) {
-        left_idx = (uintptr_t)range.left >> ((max_level - 1 - level) * TREE_WIDTH);
-        left_idx &= TREE_MASK;
-    }
-    if (right_bound_in_node) {
-        right_idx = (uintptr_t)range.right >> ((max_level - 1 - level) * TREE_WIDTH);
-        right_idx &= TREE_MASK;
-    }
+    int left_idx = _get_idx_in_node(range.left, level, parent_idx_bits);
+    int right_idx = _get_idx_in_node(range.right, level, parent_idx_bits);
 
     uintptr_t ans;
     struct radix_tree* child_array = cur->slot.children;
@@ -77,7 +86,7 @@ void* _lookup_entry(void* addr, struct radix_tree* cur, int level) {
     if (cur == NULL) {
         return NULL;
     }
-    if (cur->child_cnt != 0) {
+    if (!cur->has_children) {
         return cur->slot.data;
     }
 
@@ -97,17 +106,18 @@ void* lookup_entry(void* addr, struct radix_tree* root) {
 
 // Returns 0 on success, -1 on error
 // TODO: return MEMERR on malloc failure to distinguish between causes of failure
-int _insert_entry(void* entry, struct radix_tree* cur, int level) {
+// TODO: need to have cleanup mechanism in case insertion fails halfway
+int _insert_entry(void* entry, struct radix_tree* cur, int level, uint parent_idx_bits) {
     // Leaf Node
-    if (cur->child_cnt != 0) {
+    if (!cur->has_children) {
         if (cur->slot.data== NULL) {
             cur->slot.data = entry;
             return 0;
         }
 
         void* existing_entry = cur->slot.data;
-        struct range* existing_entry_range = entry_range(existing_entry);
-        struct range* new_entry_range = entry_range(entry);
+        struct range* existing_entry_range = _get_entry_range(existing_entry);
+        struct range* new_entry_range = _get_entry_range(entry);
 
         bool overlap = ranges_overlap(existing_entry_range, new_entry_range);
         assert(!overlap);
@@ -120,21 +130,31 @@ int _insert_entry(void* entry, struct radix_tree* cur, int level) {
             return -1;
         }
 
-        cur->child_cnt++;
+        cur->has_children = true;
         cur->slot.children = children_array;
         memset(children_array, 0, array_size);
 
-        // _insert_entry(existing_entry, children_array, level);
-        return _insert_entry(existing_entry, children_array, level);
+        _insert_entry(existing_entry, cur, level, parent_idx_bits);
+        return _insert_entry(entry, cur, level, parent_idx_bits);
     }
 
-    return NULL;
+    struct range* entry_range = _get_entry_range(entry);
+    struct range idx_range = _get_idx_range(*entry_range, level, parent_idx_bits);
+    struct radix_tree* child_arr = cur->slot.children;
+    int succ = 0;
+    for (int i = idx_range.left; i <= idx_range.right; i++) {
+        int child_parent_bits = (parent_idx_bits << TREE_WIDTH) | i;
+        succ |= _insert_entry(entry, &child_arr[i], level + 1, child_parent_bits);
+    }
+
+    return succ;
 }
 
+// Return 0 on success, -1 on error
 int insert_entry(void* entry, struct radix_tree* root) {
-    struct range* new_entry_range = entry_range(entry);
+    struct range* new_entry_range = _get_entry_range(entry);
     if (is_empty_range(*new_entry_range, root)) {
         return -1;
     }
-    return _insert_entry(entry, root, 0);
+    return _insert_entry(entry, root, 0, 0);
 }
